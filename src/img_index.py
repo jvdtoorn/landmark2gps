f9c25ae595845e0f626aaddfb87074889b4a1fe7
Jules van der Toorn <J.vanderToorn@student.tudelft.nl>
2020-03-28 13:20:33 +0100

    Implemented full landmark2gps algorithm

--- src/img_index.py
+++ src/img_index.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 from GPSPhoto import gpsphoto
 import progressbar
 import os.path
+import logging
 import imutils
 import pickle
-import glob
 import cv2
 
 # Setting this flag to 'True' will only index images with known GPS coordinates
-SKIP_UNKNOWN_GEO = False
+SKIP_UNKNOWN_GEO = True
 
 def get_meta_gps(img_path):
     try: gps_data = gpsphoto.getGPSData(img_path)
@@ -42,37 +42,52 @@ def get_input_gps():
     except ValueError: return None, None
     return float(parts[0]), float(parts[1])
 
+def find_pictures_rdir(dir_path):
+    files = []
+    for r, d, f in os.walk(dir_path):
+        for file in f:
+            if '.jpg' in file.lower() or '.jpeg' in file.lower() or '.png' in file.lower():
+                files.append(os.path.join(r, file))
+    return files
+
 def load_img_index():
 
     # If image index already exists, return it
-    if os.path.isfile('img_index.db'):
-        with open('img_index.db', 'rb') as handle:
+    if os.path.isfile('../index/img_index.db'):
+        with open('../index/img_index.db', 'rb') as handle:
             return pickle.load(handle)
 
     # Create the image index:
     # - if metadata contains GPS: use those coordinates
     # - if image is taken at Oude Jan, Nieuwe Kerk or Raadhuis: use hardcoded coordinates
     # - otherwise, ask the user to provide GPS coordinates
+    print("Please provide an image dataset.")
+    dir_path = raw_input("Directory path: ")
+    while not os.path.exists(dir_path):
+        print("This directory does not exist. Please check your spelling.")
+        dir_path = raw_input("Directory path: ")
     print("Indexing images...")
-    img_files = glob.glob("../landmarks/*/*.*")
+    img_files = find_pictures_rdir(dir_path)
     img_index = {'path': [], 'lat': [], 'long': []}
+    logging.disable(logging.CRITICAL)
     if SKIP_UNKNOWN_GEO: bar = progressbar.ProgressBar(maxval=len(img_files), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]); bar.start()
     for i in range(len(img_files)):
         if False and i > 0: print("[path: {}, lat: {}, long: {}]".format(img_index['path'][i-1], img_index['lat'][i-1], img_index['long'][i-1]))
         if SKIP_UNKNOWN_GEO: bar.update(i)
+        filename = img_files[i].split('/')[-1]
 
         # Try getting GPS from metadata
         lat, lon = get_meta_gps(img_files[i])
         if lat is not None and lon is not None:
-            img_index['path'].append(img_files[i])
+            img_index['path'].append(filename)
             img_index['lat'].append(lat)
             img_index['long'].append(lon)
             continue
 
         # Try getting hardcoded GPS from file name
-        lat, lon = get_hardcoded_gps(img_files[i].split('/')[-1])
+        lat, lon = get_hardcoded_gps(filename)
         if lat is not None and lon is not None:
-            img_index['path'].append(img_files[i])
+            img_index['path'].append(filename)
             img_index['lat'].append(lat)
             img_index['long'].append(lon)
             continue
@@ -80,7 +95,7 @@ def load_img_index():
         if SKIP_UNKNOWN_GEO: continue
         # If those didn't work, ask user for the coordinates
         print("Where is the following picture taken? (see external window)")
-        print("Name: '{}'".format(img_files[i].split('/')[-1]))
+        print("Name: '{}'".format(filename))
         img = imutils.resize(cv2.cvtColor(cv2.imread(img_files[i]), cv2.COLOR_BGR2RGB), width=900)
         plt.imshow(img); plt.show(block=False); plt.pause(0.001)
         lat, lon = get_input_gps()
@@ -88,14 +103,16 @@ def load_img_index():
             print("Invalid coordinates format, please try again.")
             lat, lon = get_input_gps()
         plt.close('all')
-
-        img_index['path'].append(img_files[i])
+        img_index['path'].append(filename)
         img_index['lat'].append(lat)
         img_index['long'].append(lon)
     if SKIP_UNKNOWN_GEO: bar.finish()
 
     # Store and return the image index
-    with open('img_index.db', 'wb') as handle: pickle.dump(img_index, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    if not os.path.exists('../index'): os.makedirs('../index')
+    with open('../index/img_index.db', 'wb') as handle: pickle.dump(img_index, handle, protocol=pickle.HIGHEST_PROTOCOL)
     return img_index
 
-if __name__ == "__main__": load_img_index()
+if __name__ == "__main__":
+    img_index = load_img_index()
+    print("Loaded image index! [{}, {}, {}]".format(len(img_index['path']), len(img_index['lat']), len(img_index['long'])))
--- src/main.py
+++ src/main.py
@@ -1,60 +1,67 @@
-import numpy as np
+# External dependencies
 import cv2
-from video_tools import *
-import glob
-import feature_extraction as ft
-from scikits.talkbox.features import mfcc
-import scipy.io.wavfile as wav
-import time
-import calendar
+import os.path
+import numpy as np
+import progressbar
+from LatLon import *
+from img_index import load_img_index
+from orb_index import load_orb_index
 
-if __name__ == "__main__": 
-	S = 0
-	E = 3
+# Configures the amount of frames processed from the input video
+USE_KEYPOINTS = 2
 
-	video_files = glob.glob("../queries/*.3gp")
+def gps_str(latitude, longitude):
+	location = LatLon(latitude, longitude)
+	pp_loc_pt = location.to_string('d%|%m%|%S%|%H')
+	res = []
+	for cor in pp_loc_pt:
+		part = cor.split('|')
+		res.append(part[0] + u'\u00b0' + part[1].zfill(2) + '\'' + part[2] + '"' + part[3])
+	return res[0] + " " + res[1]
 
-	video = video_files[0]
+def landmark2gps():
 
-	frame_count = get_frame_count(video) + 1
-	frame_rate = get_frame_rate(video)
+	# Load index
+	img_index = load_img_index()
+	orb_index = load_orb_index(img_index)
 
-	cap = cv2.VideoCapture(video)
+	# Ask for input video
+	print("Please provide a query video.")
+	video_path = raw_input("Video path: ")
+	while not os.path.isfile(video_path):
+		print("This video does not exist. Please check your spelling.")
+		video_path = raw_input("Video path: ")
 
-	prev_frame = None
+	# Calculate ORB descriptors of query video
+	orb = cv2.ORB_create(256)
+	cap = cv2.VideoCapture(video_path)
+	q_orb_index = np.empty((USE_KEYPOINTS, 256, 32), dtype='float32')
+	for i, frame_frct in np.ndenumerate(np.linspace(0.0, 1.0, USE_KEYPOINTS + 2)[1:-1]):
+		cap.set(cv2.CAP_PROP_POS_FRAMES, int(frame_frct * cap.get(cv2.CAP_PROP_FRAME_COUNT)))
+		_, frame = cap.read()
+		_, q_desc = orb.detectAndCompute(frame, None)
+		if len(q_desc) < 256: q_desc = np.vstack((q_desc, np.zeros((256 - len(q_desc), 32))))
+		q_orb_index[i] = q_desc
 
-	cap.set(cv2.CAP_PROP_POS_MSEC, S * 1000)
+	# Find best match
+	print("Finding match in dataset...")
+	highest_match_cnt = 0; highest_match_ind = -1
+	flann = cv2.FlannBasedMatcher(dict(algorithm=0, trees=5), dict(checks=50))
+	bar = progressbar.ProgressBar(maxval=orb_index.shape[0], widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]).start()
+	for db_i in range(orb_index.shape[0]):
+		bar.update(db_i)
+		for q_i in range(q_orb_index.shape[0]):
+			matches = flann.knnMatch(orb_index[db_i], q_orb_index[q_i], k=2)
+			match_count = 0
+			for m, n in matches:
+				if m.distance < 0.7 * n.distance: match_count += 1
+			if match_count > highest_match_cnt:
+				highest_match_cnt = match_count
+				highest_match_ind = db_i
+	bar.finish()
 
-	orb = cv2.ORB_create()
-	matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+	# Output the result
+	print("Your input video closely resembled the landmark '{}'.".format(img_index['path'][highest_match_ind].split('_')[-1].split('.')[0]))
+	print("Coordinates: " + gps_str(img_index['lat'][highest_match_ind], img_index['long'][highest_match_ind]))
 
-	while (cap.isOpened() and cap.get(cv2.CAP_PROP_POS_MSEC) < (E * 1000)):
-
-		retVal, frame = cap.read()
-
-		if retVal == False:
-			break
-
-		cv2.imshow('Video', frame)
-
-		start = time.time()
-		kp1, des1 = orb.detectAndCompute(frame, None)
-		end = time.time()
-		print('Keypoint calc. took {0:.2f} ms'.format((end - start) * 1000))
-
-		kp2, des2 = orb.detectAndCompute(frame, None)
-
-		start = time.time()
-		matches = matcher.match(des1, des2)
-		matches = sorted(matches, key = lambda x:x.distance)
-		end = time.time()
-		print('Keypoint match took {0:.2f} ms'.format((end - start) * 1000))
-
-	
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
-
-		prev_frame = frame
-
-	cap.release()
-	cv2.destroyAllWindows()
+if __name__ == "__main__": landmark2gps()
